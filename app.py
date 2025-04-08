0cc068b79a1f38ad52d5ead2502bcfedc8a61a1f
진주원 <84346055+jinjoo-lab@users.noreply.github.com>
2025-04-08 16:01:31 +0900

    [FEAT] Day 1 - HW02

--- app.py
+++ app.py
@@ -8,12 +8,7 @@ def hello():
     pod_name = os.environ.get('POD_NAME', 'unknown')
     node_name = os.environ.get('NODE_NAME', 'unknown')
     namespace = os.environ.get('POD_NAMESPACE', 'unknown')
-    return f"""
-    <h1>Hello from Downward API!</h1>
-    <p><b>Pod:</b> {pod_name}</p>
-    <p><b>Node:</b> {node_name}</p>
-    <p><b>Namespace:</b> {namespace}</p>
-    """
+    return f"Container EDU | POD Working : {pod_name} | nodename : {node_name} | namespace : {namespace}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
